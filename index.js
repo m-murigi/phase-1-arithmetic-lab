a8322b0e53ddefa233d42ba7a04f3acc7bbd8ced
Emmanuel <zacmac807@gmail.com>
2024-03-29 11:42:06 +0300

    add file

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+
+ let multiply = num1 * num2;
+
+
+let random = Math.ceil(Math.random (20))
+
+let num3 = 29;
+let num4 = 5;
+let mod = num3%num4;
+
+let max =Math.max(5, 10, 20, 15)
+max = 20
+
+// let mod = num3 % num4;
+
